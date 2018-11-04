515d425606b3fc79921548b9afedfd1329c1ca01
Martin Brugger <martin@nospam.brugger.eu>
2018-11-04 08:49:54 +0100

    do not build docker images for pull requests

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -2,8 +2,10 @@
 echo "Building docker image for branch: $TRAVIS_BRANCH"
 
 if [ "$TRAVIS_BRANCH" == "develop" ]; then
-    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+    if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+        echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-    docker build -t $DOCKER_USERNAME/paperbots:latest .
-    docker push $DOCKER_USERNAME/paperbots:latest
+        docker build -t $DOCKER_USERNAME/paperbots:latest .
+        docker push $DOCKER_USERNAME/paperbots:latest
+    fi
 fi
